7cc990bec3dffd9d864df8e7494fddceff2e4e94
lewisjb <lbobbermen@bigpond.com>
2016-08-27 14:13:35 +1000

    Started proof-of-concept
     - Still needs to handle iterations

--- README.md
+++ README.md
@@ -11,13 +11,18 @@
   No RNG is used except for pseudo-random generation of the initial conditions.
   This means that if you use the same seed and kill the same organisms, the
   same result will occur everytime.
+  Another use of pseudo-random number generation is for genetic algorithms.
+  This is because they are meant to be 'random', however the seed will be
+  the iteration of the simulation which allows for it to be 'random', but
+  will always be the same with the same conditions.
 
-## Technical
+## More info
 
   No hard-coding of events. This means no 'if all males die then species is
   extinct'.
 
   Chaos theory - Same algorithm applied over the network over iterations.
+
   Evolutionary algorithms - Each organism has 'DNA', its offspring's 'DNA' is
   determined by the parents'.
 
@@ -25,7 +30,24 @@
   other things. For example somebody could dedicate a lot of time to make it
   realistic for humans.
 
+  Agent-based social simulation. This means that the model is based off
+  multiple intelligent agents (organisms).
+
+  George Levinger's model of relationship is used for the connections between
+  agents.
+
+## Technical
+
+  Each organism is an intelligent agent, which acts as a node in the social
+  graph.
+
+  An iteration is a day. Similar to cellular automata, changes over the
+  iteration are based off the previous iteration, this means the graph is
+  copied, checks are made on the copy, and updated on the live one.
+
+  No leap years for simplicity, so 365 days in a year for 100 years means
+  36,500 iterations over the simulation.
+
 ## Difficulties
 
-  1. Intelligent organisms.
-  2. ...
+  1. Organisms meeting new organisms
---
+++ poc.py
@@ -0,0 +1,137 @@
+# Proof of concept
+import random
+
+class Society:
+    def __init__(self, agents):
+        self.agents = agents
+        self.iteration = 0
+
+    def next_iteration(self):
+        pass
+
+class AgentConnection:
+    """
+        Connection between two agents.
+        Based off George Levinger's model of relationships
+    """
+
+    STAGE_ACQUAINTANCE  = 0
+    STAGE_BUILDUP       = 1
+    STAGE_CONTINUATION  = 2
+    STAGE_DETERIORATION = 3
+    STAGE_ENDING        = 4
+
+    def __init__(self, agent):
+        self.agent = agent
+        self.stage = 0
+        
+        # The 8 basic emotions
+        self.joy = 0
+        self.trust = 0
+        self.fear = 0
+        self.surprise = 0
+        self.sadness = 0
+        self.disgust = 0
+        self.anger = 0
+        self.anticipation = 0
+
+class Agent:
+    def __init__(self, dna):
+        self.connections = [] # Connections with other agents
+        self.age = 0 # Age in iterations (days)
+
+        self.dna = dna
+        self.unpack_dna()
+
+    def unpack_dna(self):
+        sex = ord(self.dna[0])
+        personality = ord(self.dna[1])
+
+        self.sex = bool(sex)
+        self.personality = [bool(personality & (1 << i)) for i in xrange(4)]
+
+    def add_connection(self, agent):
+        self.connections.append(AgentConnection(agent))
+        agent.connections.append(AgentConnection(self))
+
+    def get_connection(self, agent):
+        for c in self.connections:
+            if c.agent == agent:
+                return c
+
+    def create_offspring(self, partner):
+        random.seed(self.age + partner.age)
+        first = None
+        second = None
+        if random.random() < 0.5:
+            # Gets the first half of this agent and second of partner
+            # XXX Don't hardcode
+            first = self.dna[0]
+            second = partner.dna[1]
+        else:
+            first = partner.dna[0]
+            second = partner.dna[1]
+
+        offspring_dna = "%c%c" % (first, second) # XXX Need new way for more dna
+        offspring = Agent(offspring_dna)
+
+        # Make connections with parents
+        offspring.add_connection(self)
+        offspring.add_connection(partner)
+
+        return offspring
+
+def generate_random_agent(seed):
+    sex = random.random() < 0.5
+    personality = [random.random() < 0.5 for _ in xrange(4)]
+    personality_int = 0
+    for i in xrange(4):
+        if personality[i]:
+            personality_int |= (1 << i)
+
+    dna = "%c%c" % (sex, personality_int) # XXX Fix dis
+    agent = Agent(dna)
+
+    return agent
+
+
+def generate_random_society(seed=None):
+    if seed == None: seed == os.urandom()
+    num_agents = random.randrange(10, 21)
+
+    agents = []
+    for _ in xrange(num_agents):
+        agents.append(generate_random_agent(seed))
+
+    num_connections = random.randrange(num_agents - 5, num_agents + 6)
+
+    while num_connections > 0:
+        a1 = agents[random.randrange(0, num_agents)]
+        a2 = agents[random.randrange(0, num_agents)]
+
+        if a1 != a2:
+            c1 = a1.get_connection(a2)
+            if c1:
+                c2 = a2.get_connection(a1)
+                c1.stage += 1
+                c2.stage += 1
+            else:
+                a1.add_connection(a2)
+
+        num_connections -= 1
+
+    society = Society(agents)
+
+    return society
+
+
+# How do people meet new people?
+# How do connections change?
+
+"""
+DNA Spec:
+    Sex (1 = Male, 0 = Female)
+    Myer-Briggs
+
+
+"""
